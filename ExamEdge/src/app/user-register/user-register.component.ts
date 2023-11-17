d0e2db0964949ccba7f2be26401271bda1edebce
Harshad2799 <harshad.jawale99@gmail.com>
2023-11-17 09:28:19 +0530

    registration conflict removed

--- ExamEdge/src/app/user-register/user-register.component.ts
+++ ExamEdge/src/app/user-register/user-register.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
-import {HttpClient} from '@angular/common/http';
+
 
 import { Router } from '@angular/router';
 
@@ -15,25 +15,12 @@ import { Router } from '@angular/router';
 
 export class UserRegisterComponent {
 
-  data:any
-user: details = new details();
-  constructor(private http: HttpClient){}
-  registration(){
-    let url=`http://localhost:8080/addstudent`
-    this.http.post(url,this.user,{responseType:'text'}).subscribe(data => {
-      this.data = data;
-    })
-    console.log(this.user)
-
   detail: Detail = new Detail()
   constructor(private http: HttpClient, private route: Router){}
   data!: any
   msg!: String
   file: File | undefined;
   registration(){
-
-
-    
     let url=`http://localhost:8080/addstudent`;
     this.http.post<any>(url, this.detail).subscribe(data => {
       if(data.status==true){
@@ -44,10 +31,6 @@ user: details = new details();
     })
 
     
-  }
-
-  handleFileInput(event: any): void {
-    this.file = event.target.files[0];
   }
 }
 
