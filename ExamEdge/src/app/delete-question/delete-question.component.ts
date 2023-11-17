0d9beaea5b16f3884e383444efdf67c85f1fe8b0
Pranjalr-06 <raibolrpranjal@gmail.com>
2023-11-17 18:52:22 +0530

    delete integration half

--- ExamEdge/src/app/delete-question/delete-question.component.ts
+++ ExamEdge/src/app/delete-question/delete-question.component.ts
@@ -9,35 +9,58 @@ import { Component } from '@angular/core';
 export class DeleteQuestionComponent {
   
   selectedSubject!: String;
-  subjectlist: string[] = [];
-  selectedQuestions: any[] = [];
+  subjectlist: subject[] = [];
+  selectedQuestions: any = [];
   name!: String
   questions: any[] = []
+  id: number | undefined
+  sub: subject =new subject();
   constructor(private http: HttpClient){}
   
-  OnInit(){
-    this.subjectdropdown;
+  ngOnInit(){
+    this.subjectdropdown();
   }
   subjectdropdown() {
     let url = `http://localhost:8080/allsubjects`
     this.http.get<any>(url).subscribe(data => {
       this.subjectlist = data
+      console.log(this.subjectlist)
     })
   }
   SubjectSelect() {
+
     // console.log('Selected Subject:', this.selectedSubject);
-    this.selectedQuestions = this.questions.filter(question => question.subject === this.selectedSubject);
+   this.selectedQuestions = this.questions.filter(question => question.subject === this.selectedSubject);
     // console.log('Selected Questions:', this.selectedQuestions);
+   // let url=`http://localhost:8080/fetchallquestion/${this.id}`
+    this.id = this.sub.id
+    console.log(this.id)
+    //this.http.get(url).subscribe(data =>{
+
+    //})
   }
 
   onDelete() {
-    this.selectedQuestions = [];
+    let url=`http://localhost:8080/deletequestion/{id}`
+    this.http.post(url, this.selectedQuestions).subscribe(data =>{
+      this.selectedQuestions=data
+    })
   }
 
   onDeleteQuestion(index: number): void {
     alert("Are you sure you want to delete");
     this.selectedQuestions.splice(index, 1);
   }
+
+  
 }
+
+export class subject{
+  name!: String
+  id: number | undefined
+}
+
+
+
 
 
